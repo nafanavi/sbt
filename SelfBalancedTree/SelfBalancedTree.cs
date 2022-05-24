ec99da34972eda8fd339902f60781ab572324d6b
Ivan <aivyu@rambler.ru>
2022-05-24 13:08:18 +0300

    change and move sbt function to seperate class

--- SelfBalancedTree/SelfBalancedTree.cs
+++ SelfBalancedTree/SelfBalancedTree.cs
@@ -1,5 +1,121 @@
 namespace SelfBalancedTree
 {
+
+    public class SBT_Actions
+    {
+        public TreeNode balance(TreeNode node)
+        {
+            if (node.balanceFactor < -1)
+            {
+                if (node.right!.balanceFactor > 1)
+                {
+                    return rotateRightLeft(node);
+                }
+                else
+                {
+                    return rotateLeft(node);
+                }
+            }
+            if (node.balanceFactor > 1)
+            {
+                if (node.left!.balanceFactor < -1)
+                {
+                    return rotateLeftRight(node);
+                }
+                else
+                {
+                    return rotateRight(node);
+                }
+            }
+            return node;
+        }
+
+        public TreeNode insert(TreeNode root, int value)
+        {
+            if (value > root.value)
+            {
+                if (root.right != null)
+                {
+                    root.right = insert(root.right, value);
+                }
+                else
+                {
+                    root.right = new TreeNode(value, null, null);
+                }
+
+            }
+            else
+            {
+                if (root.left != null)
+                {
+                    root.left = insert(root.left, value);
+                }
+                else
+                {
+                    root.left = new TreeNode(value, null, null);
+                }
+            }
+            return root;
+        }
+
+        public TreeNode insert(TreeNode root, TreeNode newNode)
+        {
+            if (newNode.value > root.value)
+            {
+                if (root.right != null)
+                {
+                    root.right = insert(root.right, newNode);
+                }
+                else
+                {
+                    root.right = newNode;
+                }
+
+            }
+            else
+            {
+                if (root.left != null)
+                {
+                    root.left = insert(root.left, newNode);
+                }
+                else
+                {
+                    root.left = newNode;
+                }
+            }
+            return balance(root);
+        }
+
+        public TreeNode rotateRight(TreeNode node)
+        {
+            var buff = node;
+            node = buff.left!;
+            buff.left = node.right;
+            node!.right = buff;
+            return node;
+        }
+
+        public TreeNode rotateLeft(TreeNode node)
+        {
+            var buff = node;
+            node = buff.right!;
+            buff.right = node.left;
+            node!.left = buff;
+            return node;
+        }
+
+        public TreeNode rotateRightLeft(TreeNode node)
+        {
+            node = rotateRight(node.right!);
+            return rotateLeft(node);
+        }
+
+        public TreeNode rotateLeftRight(TreeNode node)
+        {
+            node = rotateLeft(node.left!);
+            return rotateRight(node);
+        }
+    }
     public class TreeNode
     {
         public TreeNode(int value, TreeNode? left, TreeNode? right)
@@ -15,119 +131,7 @@ namespace SelfBalancedTree
 
         public int balanceFactor { get { return (this.left?.height ?? 0) - (this.right?.height ?? 0); } }
 
-        public static void balance(ref TreeNode node)
-        {
-            if (node.balanceFactor < -1)
-            {
-                if (node.right!.balanceFactor > 1)
-                {
-                    rotateRightLeft(ref node);
-                }
-                else
-                {
-                    rotateLeft(ref node);
-                }
-            }
-            if (node.balanceFactor > 1)
-                if (node.left!.balanceFactor < -1)
-                {
-                    rotateLeftRight(ref node);
-                }
-                else
-                {
-                    rotateRight(ref node);
-                }
-        }
 
-        public void insert(int value)
-        {
-            if (value > this.value)
-            {
-                if (this.right != null)
-                {
-                    this.right.insert(value);
-                }
-                else
-                {
-                    this.right = new TreeNode(value, null, null);
-                }
-
-            }
-            else
-            {
-                if (this.left != null)
-                {
-                    this.left.insert(value);
-                }
-                else
-                {
-                    this.left = new TreeNode(value, null, null);
-                }
-            }
-        }
-
-        public void insert(TreeNode node)
-        {
-            if (node.value > this.value)
-            {
-                if (this.right != null)
-                {
-                    this.right.insert(node);
-                }
-                else
-                {
-                    this.right = node;
-                }
-
-            }
-            else
-            {
-                if (this.left != null)
-                {
-                    this.left.insert(node);
-                }
-                else
-                {
-                    this.left = node;
-                }
-            }
-        }
-
-        public static void rotateRight(ref TreeNode root)
-        {
-            var buff = root;
-            root = buff.left!;
-            buff.left = root.right;
-            root!.right = buff;
-        }
-
-        public static void rotateLeft(ref TreeNode root)
-        {
-            var buff = root;
-            root = buff.right!;
-            buff.right = root.left;
-            root!.left = buff;
-        }
-
-        public void rotateLeft()
-        {
-            // var buff = this;
-            // this = buff.right!;
-            // buff.right = root.left;
-            // root!.left = buff;
-        }
-
-        public static void rotateRightLeft(ref TreeNode root)
-        {
-            rotateRight(ref root.right!);
-            rotateLeft(ref root);
-        }
-
-        public static void rotateLeftRight(ref TreeNode root)
-        {
-            rotateLeft(ref root.left!);
-            rotateRight(ref root);
-        }
 
 
     }
