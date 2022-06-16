a71e6f4ef85b8077e702db4db161529e1619adbf
Ivan <aivyu@rambler.ru>
2022-06-16 17:30:57 +0300

    refactore DrawCircle, drawLine and DrawLabel; play with dragdrop

--- sbt-ui/SPTVisualisationApp/WpfApp1/MainWindow.xaml.cs
+++ sbt-ui/SPTVisualisationApp/WpfApp1/MainWindow.xaml.cs
@@ -5,6 +5,8 @@ using System.Windows.Shapes;
 using SelfBalancedTree;
 using TreeNode = SelfBalancedTree.TreeNode;
 using Point = System.Windows.Point;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace WpfApp1
 {
@@ -40,43 +42,76 @@ namespace WpfApp1
         private const double circleRadius = 20.0;
         private const double initialNodesDistanceX = 10 * circleRadius;
         private const double initialNodesDistanceY = 2.5 * circleRadius;
-        private void DrawCircle(Point coords)
+        private void InitCircle(Point coords)
         {
             var circle = new Ellipse();
+            MyCanvas.Children.Add(circle);
             circle.Height = 2 * circleRadius;
             circle.Width = circle.Height;
             circle.Stroke = System.Windows.Media.Brushes.Black;
-            MyCanvas.Children.Add(circle);
+            circle.Fill = System.Windows.Media.Brushes.Aqua;
+            circle.MouseMove += new MouseEventHandler(CircleMoveHandler);
+            SetCircleCoord(circle, coords);
+        }
+        private void CircleMoveHandler(object sender, MouseEventArgs e)
+        {
+            Ellipse circle = sender as Ellipse;
+            if (circle != null && e.LeftButton == MouseButtonState.Pressed)
+            {
+                var point = e.GetPosition(MyCanvas);
+                SetCircleCoord(circle, point);
+                //DragDrop.DoDragDrop(circle,
+                //    circle.Fill.ToString(),
+                //     DragDropEffects.Move);
+            }
+        }
+
+        private void SetCircleCoord(Ellipse circle, Point coords)
+        {
             Canvas.SetLeft(circle, coords.X - circleRadius);
             Canvas.SetTop(circle, coords.Y - circleRadius);
         }
 
-        private void DrawLabel(string value, Point coords)
+        private void SetLabelCoords(Label label, Point coords)
         {
-            var label = new System.Windows.Controls.Label();
-            label.Content = value;
-            MyCanvas.Children.Add(label);
             Canvas.SetLeft(label, coords.X - circleRadius / 2);
             Canvas.SetTop(label, coords.Y - circleRadius / 2);
         }
 
-        private void DrawLine(Point point1, Point point2)
+
+        private void InitLabel(string value, Point coords)
+        {
+            var label = new Label();
+            label.Content = value;
+            MyCanvas.Children.Add(label);
+            SetLabelCoords(label, coords);
+        }
+
+        private void InitLine(Point point1, Point point2)
         {
             var line = new Line();
             line.Stroke = System.Windows.Media.Brushes.Black;
+            SetLineCoords(line, point1, point2);
+            MyCanvas.Children.Add(line);
+        }
+
+
+        private void SetLineCoords(Line line, Point point1, Point point2)
+        {
+            //MyCanvas.Children.Remove(line);
             line.X1 = point1.X;
             line.Y1 = point1.Y;
             line.X2 = point2.X;
             line.Y2 = point2.Y;
-            MyCanvas.Children.Add(line);
+            //MyCanvas.Children.Add(line);
         }
 
         private void DrawTree(TreeNode node, Point nodeCoords)
         {
             if (node != null)
             {
-                DrawCircle(nodeCoords);
-                DrawLabel(node.value.ToString(), nodeCoords);
+                InitCircle(nodeCoords);
+                InitLabel(node.value.ToString(), nodeCoords);
                 var spreadingFactor = rootCoord.Y / nodeCoords.Y;
                 var nodeDeltaX = initialNodesDistanceX * spreadingFactor;
                 var nodeDeltaY = initialNodesDistanceY;
@@ -87,7 +122,7 @@ namespace WpfApp1
                 {
                     var leftNodeX = nodeCoords.X - nodeDeltaX;
                     var leftNodeY = nodeCoords.Y + nodeDeltaY;
-                    DrawLine(
+                    InitLine(
                         new Point(nodeCoords.X - lineDeltaX, nodeCoords.Y + lineDeltaY),
                         new Point(leftNodeX + lineDeltaX, leftNodeY - lineDeltaY)
                     );
@@ -97,7 +132,7 @@ namespace WpfApp1
                 {
                     var rightNodeX = nodeCoords.X + nodeDeltaX;
                     var rightNodeY = nodeCoords.Y + initialNodesDistanceY;
-                    DrawLine(
+                    InitLine(
                         new Point(nodeCoords.X + lineDeltaX, nodeCoords.Y + lineDeltaY),
                         new Point(rightNodeX - lineDeltaX, rightNodeY - lineDeltaY)
                     );
@@ -112,5 +147,107 @@ namespace WpfApp1
             MyCanvas.Children.Clear();
             sbt = new TreeNode(1, null, null);
         }
+
+        private void ellipse_MouseMove(object sender, MouseEventArgs e)
+        {
+            Ellipse ellipse = sender as Ellipse;
+            if (ellipse != null && e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragDrop.DoDragDrop(ellipse,
+                                     ellipse.Fill.ToString(),
+                                     DragDropEffects.Copy);
+            }
+        }
+
+
+        private void ellipse_MouseMove_Test(object sender, MouseEventArgs e)
+        {
+            Ellipse ellipse = sender as Ellipse;
+            if (ellipse != null && e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragDrop.DoDragDrop(ellipse,
+                                     ellipse.Fill.ToString(),
+                                     DragDropEffects.Move);
+            }
+        }
+
+        private Brush previousFill = null;
+        //private void ellipse_DragEnter(object sender, DragEventArgs e)
+        //{
+        //    Ellipse ellipse = sender as Ellipse;
+        //    if (ellipse != null)
+        //    {
+        //        // Save the current Fill brush so that you can revert back to this value in DragLeave.
+        //        _previousFill = ellipse.Fill;
+
+        //        // If the DataObject contains string data, extract it.
+        //        if (e.Data.GetDataPresent(DataFormats.StringFormat))
+        //        {
+        //            string dataString = (string)e.Data.GetData(DataFormats.StringFormat);
+
+        //            // If the string can be converted into a Brush, convert it.
+        //            BrushConverter converter = new BrushConverter();
+        //            if (converter.IsValid(dataString))
+        //            {
+        //                Brush newFill = (Brush)converter.ConvertFromString(dataString);
+        //                ellipse.Fill = newFill;
+        //            }
+        //        }
+        //    }
+        //}
+
+        private void ellipse_DragOver(object sender, DragEventArgs e)
+        {
+            //e.Effects = DragDropEffects.None;
+            var circle = e.Source as Ellipse;
+            if (circle != null)
+            {
+                SetCircleCoord(circle, e.GetPosition(MyCanvas));
+            }
+            // If the DataObject contains string data, extract it.
+            //if (e.Data.GetDataPresent(DataFormats.StringFormat))
+            //{
+            //    string dataString = (string)e.Data.GetData(DataFormats.StringFormat);
+
+            //    // If the string can be converted into a Brush, allow copying.
+            //    BrushConverter converter = new BrushConverter();
+            //    if (converter.IsValid(dataString))
+            //    {
+            //        e.Effects = DragDropEffects.Copy | DragDropEffects.Move;
+            //    }
+            //}
+        }
+
+        //private void ellipse_DragLeave(object sender, DragEventArgs e)
+        //{
+        //    Ellipse ellipse = sender as Ellipse;
+        //    if (ellipse != null)
+        //    {
+        //        ellipse.Fill = _previousFill;
+        //    }
+        //}
+
+        //private void ellipse_Drop(object sender, DragEventArgs e)
+        //{
+        //    Ellipse ellipse = sender as Ellipse;
+        //    if (ellipse != null)
+        //    {
+        //        // If the DataObject contains string data, extract it.
+        //        if (e.Data.GetDataPresent(DataFormats.StringFormat))
+        //        {
+        //            string dataString = (string)e.Data.GetData(DataFormats.StringFormat);
+
+        //            // If the string can be converted into a Brush,
+        //            // convert it and apply it to the ellipse.
+        //            BrushConverter converter = new BrushConverter();
+        //            if (converter.IsValid(dataString))
+        //            {
+        //                Brush newFill = (Brush)converter.ConvertFromString(dataString);
+        //                ellipse.Fill = newFill;
+        //            }
+        //        }
+        //    }
+        //}
+
     }
 }
